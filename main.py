0025fc2dd59ee9d4e8789b3eab3bb08a843f7a07
Whipander <mihaja2506@gmail.com>
2025-07-15 21:53:14 +0300

    feat: add type checking for accept

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import json
-from fastapi import FastAPI
-from starlette.responses import Response
+from urllib.request import Request
+
+from fastapi import FastAPI, requests
+from starlette.responses import Response, JSONResponse
 
 app = FastAPI()
 
 
 @app.get("/")
-def root():
+def root(request: requests.Request):
+    accept_header = request.headers.get("Accept")
+    if accept_header != "text/html" and accept_header != "text/plain":
+        return JSONResponse(content={
+            "message": """Le type de format attendu n'est pas supporté. Seul les types '`text/html' et 'text/plain' sont supportés."""},
+                            status_code=400)
     with open("welcome.html", "r", encoding="utf-8") as file:
         html_content = file.read()
     return Response(content=html_content, status_code=200, media_type="text/html")

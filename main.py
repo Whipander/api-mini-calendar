164257d458bbe58b8866ee7a84a75842c49f159f
Whipander <mihaja2506@gmail.com>
2025-07-16 18:26:02 +0300

    feat: add 404 page

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import json
-from urllib.request import Request
-
-from fastapi import FastAPI, requests
+from fastapi import FastAPI
 from starlette.responses import Response, JSONResponse
-
+from starlette.requests import Request
 app = FastAPI()
 
 
 @app.get("/")
-def root(request: requests.Request):
+def root(request: Request):
     accept_header = request.headers.get("Accept")
     x_api_key = request.headers.get("x-api-key")
 
@@ -26,5 +24,7 @@ def root(request: requests.Request):
 
 @app.get("/{full_path:path}")
 def catch_all(full_path: str):
-    not_found_message = {"detail": f"Page '/{full_path}' not found"}
-    return Response(content=json.dumps(not_found_message), status_code=404, media_type="application/json")
+    with open("404.html", "r", encoding="utf-8") as file:
+        html_content = file.read()
+    return Response(content=html_content, status_code=404, media_type="text/html")
+
